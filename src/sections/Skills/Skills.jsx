35d84680d5feca68f46266718ff88ed7b9a04691
kalix <parasshresthanever@gmail.com>
2024-06-30 09:49:28 +0545

    skill

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -13,7 +13,36 @@ function Skills() {
       <h1>Skills</h1>
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill='HTML'/>
+        <SkillList src={checkMarkIcon} skill='CSS'/>
+        <SkillList src={checkMarkIcon} skill='JAVASCRIPT'/>
+        <SkillList src={checkMarkIcon} skill='TYPESCRIPT'/>
+        <SkillList src={checkMarkIcon} skill='NODE'/>
+        
+        
       </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill='REACT'/>
+        <SkillList src={checkMarkIcon} skill='NEXT JS'/>
+        <SkillList src={checkMarkIcon} skill='TAILWIND CSS'/>
+        <SkillList src={checkMarkIcon} skill='FIGMA'/>
+        
+      
+        
+      </div>
+      <hr />
+  
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill='Redux'/>
+        <SkillList src={checkMarkIcon} skill='Git'/>
+        <SkillList src={checkMarkIcon} skill='Bootstrap'/>
+        
+        <SkillList src={checkMarkIcon} skill='C++'/>
+        <SkillList src={checkMarkIcon} skill='Python'/>
+        
+      </div>
+      
+
     </section>
   );
 }
