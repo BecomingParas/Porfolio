391b54b9a616628367704aaedf944c4a0f83baf0
kalix <parasshresthanever@gmail.com>
2024-07-04 08:34:18 +0545

    astiko

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import styles from "./ProjectsStyles.module.css";
-import viberr from "../../assets/viberr.png";
+
 import hipsster from"../../assets/hipsster.png";
 import freshBurger from "../../assets/fresh-burger.png"
 import ProjectCard from "../../common/ProjectCard";
-import fitLift from "../../assets/fitlift.png"
+import fitLift from "../../assets/fitlift.png";
+import frontend from "../../assets/frontend-hora.png"
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
-        <ProjectCard
-          src={viberr}
-          link="https://github.com/parasnever/reactPorfolio"
-          h3="Viberr"
-          p="Streaming App"
-        />
+        <ProjectCard 
+        src={frontend}
+        link="https://portfolio-demo-snowy.vercel.app/"
+        h3="Frontend Hora"
+        p="Portfolio Horaa"/>
         <ProjectCard
           src={freshBurger}
           link="https://github.com/parasnever/reactPorfolio"
