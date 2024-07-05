aeac240846a70191a8fae159a90e804849ce63f7
symbioids <parasshresthanever@gmail.com>
2024-07-05 18:04:39 +0545

    headers add garna lako

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import Footer from './sections/Footer/Footer'
 import Hero from './sections/Hero/Hero'
 import Projects from './sections/Projects/Projects'
 import Skills from './sections/Skills/Skills'
-
+import Header from './sections/Header/Header'
 function App() {
 
 
   return (
     <>
+    <Header />
     <Hero />
     <Projects />
     <Skills />
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { ThemeProvider } from './common/ThemeContext.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ThemeProvider } from "./common/ThemeContext.jsx";
+import { Route, RouterProvider, createBrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+import Hero from "./sections/Hero/Hero.jsx";
+import Projects from "./sections/Projects/Projects.jsx";
+import Contact from "./sections/Contact/Contact.jsx";
+import Skills from "./sections/Skills/Skills.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Hero />,
+      },
+      {
+        path: "projects",
+        element: <Projects />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider >
-
-    <App />
-    </ThemeProvider>
-  </React.StrictMode>,
-)
+      <ThemeProvider>
+    <RouterProvider router={router} />
+      
+      </ThemeProvider>
+  </React.StrictMode>
+);
