38d948fd2fa9dbaed45a4e265847a6693650efd0
parasnever <parasshresthanever@gmail.com>
2025-02-06 19:58:56 +0545

    Update GitHub link in Header component

--- src/sections/Header/Header.jsx
+++ src/sections/Header/Header.jsx
@@ -25,7 +25,7 @@ function Header() {
                 Linkdein
               </Link>
               <Link
-                to="https://github.com/parasnever"
+                to="https://github.com/BecomingParas"
                 className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium 
                 rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                 target="_blank"
